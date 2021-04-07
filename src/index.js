6c2bf4ef76761bfcc143047ca4a2db5521d9789e
marcus vinicius <viniciuscordeiro789@gmail.com>
2021-04-07 20:58:57 -0300

    Desafio_2_node

--- src/index.js
+++ src/index.js
@@ -10,19 +10,75 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: 'User not found' });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+
+  const { user } = request;
+
+  if (!user.pro && user.todos.length < 10) {
+
+    return next();
+
+  }
+
+  if (user.pro) {
+    return next()
+  }
+
+  return response.status(403).json({ error: 'User not found' });
+
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find((user) => user.username === username);
+  if (!user) {
+    return response.status(404).json({ error: 'User not exists' });
+
+  }
+
+  const Checkuuid = validate(id);
+  if (!Checkuuid) {
+    return response.status(400).json({ error: 'Id invalid' });
+  }
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if (!todo) {
+    return response.status(404).json({ error: 'todo not exists' });
+  }
+
+  request.todo = todo;
+  request.user = user;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+  const user = users.find(user => user.id === id);
+
+  if (!user) {
+    return response.status(404).json({ error: 'todo not exists' });
+  }
+  request.user = user;
+  return next();
+
+
 }
 
 app.post('/users', (request, response) => {
@@ -49,6 +105,7 @@ app.post('/users', (request, response) => {
 
 app.get('/users/:id', findUserById, (request, response) => {
   const { user } = request;
+
 
   return response.json(user);
 });
